b437c973932d543d58a47b6fcb40b41610e82024
Ernie <s2503901@gmail.com>
2022-01-09 21:41:05 +0800

    Add Implement Trie Prefix Tree

---
+++ Problems/Trie/0208-ImplementTrie(PrefixTree).py
@@ -0,0 +1,77 @@
+"""
+A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+
+Implement the Trie class:
+
+Trie() Initializes the trie object.
+void insert(String word) Inserts the string word into the trie.
+boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+
+
+Example 1:
+
+Input
+["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+Output
+[null, null, true, false, true, null, true]
+
+Explanation
+Trie trie = new Trie();
+trie.insert("apple");
+trie.search("apple");   // return True
+trie.search("app");     // return False
+trie.startsWith("app"); // return True
+trie.insert("app");
+trie.search("app");     // return True
+
+
+Constraints:
+
+1 <= word.length, prefix.length <= 2000
+word and prefix consist only of lowercase English letters.
+At most 3 * 10^4 calls in total will be made to insert, search, and startsWith.
+"""
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = {}
+        self.end_symbol = "*"
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for s in word:
+            if s not in node:
+                node[s] = {}
+            node = node[s]
+
+        node[self.end_symbol] = word
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for s in word:
+            if s not in node:
+                return False
+            node = node[s]
+        if "*" in node:
+            return True
+        else:
+            return False
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for s in prefix:
+            if s not in node:
+                return False
+            node = node[s]
+
+        return True
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
