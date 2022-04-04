ef9dc63181f3e00d4d06a9eb4e34c64070bf49f8
Ernie <s2503901@gmail.com>
2022-04-04 14:56:41 +0800

    Add new solution

--- Problems/LinkedList/1721-SwappingNodesInALinkedList.py
+++ Problems/LinkedList/1721-SwappingNodesInALinkedList.py
@@ -3,10 +3,7 @@ You are given the head of a linked list, and an integer k.
 
 Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
 
-
-
 Example 1:
-
 
 Input: head = [1,2,3,4,5], k = 2
 Output: [1,4,3,2,5]
@@ -62,5 +59,45 @@ class Solution:
         prev1.next = swap2
         prev2.next = swap1
         swap1.next, swap2.next = swap2.next, swap1.next
+
+        return dummy.next
+
+
+class Solution2:
+    """
+    Use value exchange
+    """
+    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        right = head
+        checker = head
+        for i in range(k-1):
+            checker = checker.next
+        left = checker
+        while checker.next:
+            checker = checker.next
+            right = right.next
+        left.val, right.val = right.val, left.val
+
+        return head
+
+
+class Solution3:
+    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        dummy = ListNode
+        dummy.next = head
+        prev_left = dummy
+        prev_right = dummy
+        right = head
+        checker = head
+        for i in range(k-1):
+            checker = checker.next
+            prev_left = prev_left.next
+        left = checker
+        while checker.next:
+            checker = checker.next
+            right = right.next
+            prev_right = prev_right.next
+        prev_left.next, prev_right.next = right, left
+        left.next, right.next = right.next, left.next
 
         return dummy.next
