63d41536331ffcfdf9f2afeff2b7a3f4612a8e57
Ernie <s2503901@gmail.com>
2022-04-19 22:44:54 +0800

    Add go 0020

---
+++ Problems/Stack/0020-ValidParentheses.go
@@ -0,0 +1,21 @@
+func isValid(s string) bool {
+    board := map[rune]rune {
+        ')': '(',
+        ']': '[',
+        '}': '{',
+    }
+    stack := []rune {}
+    for _, c := range(s) {
+        switch c {
+            case '(', '[', '{':
+                stack = append(stack, c)
+            case ')', ']', '}':
+                if len(stack) == 0 || stack[len(stack) - 1] != board[c] {
+                    return false
+                }
+                stack = stack[:len(stack) - 1]
+        }
+
+    }
+    return len(stack) == 0
+}
