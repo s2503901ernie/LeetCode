223d39a85c9df95221d0e6c0b9b21e81cff11a5d
Ernie <s2503901@gmail.com>
2022-05-14 22:37:53 +0800

    Add one to 0079

--- Problems/python/Recursion/0079-WordSearch.py
+++ Problems/python/Recursion/0079-WordSearch.py
@@ -32,6 +32,9 @@ n = board[i].length
 1 <= word.length <= 15
 board and word consists of only lowercase and uppercase English letters.
 """
+from typing import List
+
+
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         ans = {}
@@ -71,3 +74,33 @@ class Solution:
             neighbors.append([row, col + 1])
 
         return neighbors
+
+
+class Solution2:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == word[0]:
+                    ans = self.dfs(i, j, 0, board, word)
+                    if ans:
+                        return True
+        return False
+
+    def dfs(self, i, j, cur, b, word):
+        cur += 1
+        if cur == len(word):
+            return True
+        if b[i][j] == '#':
+            return False
+        tmp = b[i][j]
+        b[i][j] = '#'
+        steps = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        for step in steps:
+            row, col = i + step[0], j + step[1]
+            if 0 <= row < len(b) and 0 <= col < len(b[0]) and b[row][col] == word[cur]:
+                if self.dfs(row, col, cur, b, word):
+                    return True
+
+        b[i][j] = tmp
+
