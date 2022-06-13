77f9ba3dca90bb29a2ec49118732375bf76b93f0
Ernie <s2503901@gmail.com>
2022-06-14 00:35:51 +0800

    Add 0763

---
+++ Problems/python/Greedy/0763-PartitionLabels.py
@@ -0,0 +1,46 @@
+"""
+You are given a string s. We want to partition the string into as many parts as possible so that each letter appears in at most one part.
+
+Note that the partition is done so that after concatenating all the parts in order, the resultant string should be s.
+
+Return a list of integers representing the size of these parts.
+
+
+
+Example 1:
+
+Input: s = "ababcbacadefegdehijhklij"
+Output: [9,7,8]
+Explanation:
+The partition is "ababcbaca", "defegde", "hijhklij".
+This is a partition so that each letter appears in at most one part.
+A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
+Example 2:
+
+Input: s = "eccbbbbdec"
+Output: [10]
+
+
+Constraints:
+
+1 <= s.length <= 500
+s consists of lowercase English letters.
+"""
+from typing import List
+
+
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        d = {}
+        for i in range(len(s)):
+            d[s[i]] = i
+        left = 0
+        right = 0
+        ans = []
+        for i in range(len(s)):
+            right = max(right, d[s[i]])
+            if i == right:
+                ans.append(right - left + 1)
+                left = i + 1
+
+        return ans
