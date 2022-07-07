e3e905398defc21edf4abc745260b532b13f0e69
Ernie <s2503901@gmail.com>
2022-07-08 00:19:37 +0800

    add 220707

---
+++ Problems/python/DynamicProgramming/0097-InterleavingString.py
@@ -0,0 +1,54 @@
+"""
+Given strings s1, s2, and s3, find whether s3 is formed by an interleaving of s1 and s2.
+
+An interleaving of two strings s and t is a configuration where they are divided into non-empty substrings such that:
+
+s = s1 + s2 + ... + sn
+t = t1 + t2 + ... + tm
+|n - m| <= 1
+The interleaving is s1 + t1 + s2 + t2 + s3 + t3 + ... or t1 + s1 + t2 + s2 + t3 + s3 + ...
+Note: a + b is the concatenation of strings a and b.
+
+
+
+Example 1:
+
+
+Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+Output: true
+Example 2:
+
+Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+Output: false
+Example 3:
+
+Input: s1 = "", s2 = "", s3 = ""
+Output: true
+
+
+Constraints:
+
+0 <= s1.length, s2.length <= 100
+0 <= s3.length <= 200
+s1, s2, and s3 consist of lowercase English letters.
+
+
+Follow up: Could you solve it using only O(s2.length) additional memory space?
+"""
+
+
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        l1, l2, l3 = len(s1), len(s2), len(s3)
+        if l1 + l2 != l3:
+            return False
+        dp = [[False for _ in range(l2 + 1)] for _ in range(l1 + 1)]
+        dp[0][0] = True
+        for i in range(0, l1 + 1):
+            for j in range(l2 + 1):
+                if i > 0 and s1[i - 1] == s3[i + j - 1] and dp[i - 1][j]:
+                    dp[i][j] = True
+                if j > 0 and s2[j - 1] == s3[i + j -1] and dp[i][j - 1]:
+                    dp[i][j] = True
+
+        return dp[-1][-1]
