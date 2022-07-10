f3a201f36a241adc775405476de859515f4e51e3
Ernie <s2503901@gmail.com>
2022-07-10 23:45:10 +0800

    add 220710

---
+++ Problems/python/DynamicProgramming/0746-MinCostClimbingStairs.py
@@ -0,0 +1,60 @@
+"""
+You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+
+You can either start from the step with index 0, or the step with index 1.
+
+Return the minimum cost to reach the top of the floor.
+
+
+
+Example 1:
+
+Input: cost = [10,15,20]
+Output: 15
+Explanation: You will start at index 1.
+- Pay 15 and climb two steps to reach the top.
+The total cost is 15.
+Example 2:
+
+Input: cost = [1,100,1,1,1,100,1,1,100,1]
+Output: 6
+Explanation: You will start at index 0.
+- Pay 1 and climb two steps to reach index 2.
+- Pay 1 and climb two steps to reach index 4.
+- Pay 1 and climb two steps to reach index 6.
+- Pay 1 and climb one step to reach index 7.
+- Pay 1 and climb two steps to reach index 9.
+- Pay 1 and climb one step to reach the top.
+The total cost is 6.
+
+
+Constraints:
+
+2 <= cost.length <= 1000
+0 <= cost[i] <= 999
+"""
+from typing import List
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        ans = [float('inf') for _ in range(len(cost) + 1)]
+        ans[0] = 0
+        for i in range(1, len(cost) + 1):
+            if i == 1:
+                ans[i] = cost[i - 1]
+            else:
+                ans[i] = min(ans[i - 1], ans[i - 2]) + cost[i - 1]
+
+        return min(ans[-2:])
+
+
+class Solution2:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        pre1 = cost[0]
+        pre2 = cost[1]
+        for i in range(2, len(cost)):
+            cur = min(pre1, pre2) + cost[i]
+            pre1, pre2 = pre2, cur
+
+        return min(pre1, pre2)
